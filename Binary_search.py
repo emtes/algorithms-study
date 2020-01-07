97ab24ecc93b098edbdaa28a4bbd1f119b483ca4
Enmanuel dlN <edelanuez.lang@gmail.com>
2020-01-07 00:59:43 +0000

    remove step counter, print test invocations

--- Binary_search.py
+++ Binary_search.py
@@ -2,26 +2,21 @@ def binary_search(lst, item):
     # define initial high and low
     low = 0
     high = len(lst) - 1
-    ### lemme see steps for fun/stress testing
-    step = 0
 
-    # loop while low and high aren't the same element
+    # loop while low and high aren't the same
     while low <= high:
-        ####
-        step += 1
-        ####
         # define a middle and guess by accessing list
         mid = (low + high) // 2
         guess = lst[mid]
         # did we guess correctly?
         if guess == item:
-            print(step)
             return mid
 
         # are we too low?
         if guess < item:
             # then new low must be at least one above our guess, numbered by mid
             low = mid + 1
+
         # or are we too high?
         else:
             # then new high must be at least one below our guess
@@ -32,6 +27,6 @@ def binary_search(lst, item):
 # Testing
 sorted_list = list(range(75))
 
-binary_search(sorted_list, 6)
-binary_search(sorted_list, 12)
-binary_search(sorted_list, 11)
+print(binary_search(sorted_list, 6))
+print(binary_search(sorted_list, 67))
+print(binary_search(sorted_list, 52))
